9a48c628972a349671d3090fbb35ae5d1b999ab7
kimwanyoung <dhks2869@gmail.com>
2024-05-09 18:33:04 +0900

    feat: pipe test위한 parse-pokemon-id.pipe.ts 구현

---
+++ src/parse-pokemon-id/parse-pokemon-id.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParsePokemonIdPipe } from './parse-pokemon-id.pipe';
+
+describe('ParsePokemonIdPipe', () => {
+  it('should be defined', () => {
+    expect(new ParsePokemonIdPipe()).toBeDefined();
+  });
+});
