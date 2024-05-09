7c36121361d3ed94f1941708510ad9a3aa51543a
kimwanyoung <dhks2869@gmail.com>
2024-05-09 17:56:38 +0900

    feat : create tweet test

--- src/tweets/tweets.service.spec.ts
+++ src/tweets/tweets.service.spec.ts
@@ -12,7 +12,32 @@ describe('TweetsService', () => {
     service = module.get<TweetsService>(TweetsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createTweet', () => {
+    it('should create tweet', () => {
+      //given
+      service.tweets = [];
+      const payload = 'This is my tweet';
+
+      //when
+      const tweet = service.createTweet(payload);
+
+      //then
+      expect(tweet).toBe(payload);
+      expect(service.tweets).toHaveLength(1);
+    });
+
+    it('should prevent tweets created which are over 100 characters', () => {
+      //given
+      const payload =
+        'This is a long tweet over 100 characters This is a long tweet over 100 characters This is a long t...';
+
+      //when
+      const tweet = () => {
+        return service.createTweet(payload);
+      };
+
+      //then
+      expect(tweet).toThrowError();
+    });
   });
 });
