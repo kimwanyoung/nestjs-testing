c5bc1b173bd1629d21417902492ef28f1dd9e502
kimwanyoung <dhks2869@gmail.com>
2024-05-09 18:17:37 +0900

    test : http api request test

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
+import { HttpModule } from '@nestjs/axios';
+import { BadRequestException } from "@nestjs/common";
 
 describe('PokemonService', () => {
-  let service: PokemonService;
+  let pokemonService: PokemonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [PokemonService],
     }).compile();
 
-    service = module.get<PokemonService>(PokemonService);
+    pokemonService = module.get<PokemonService>(PokemonService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(pokemonService).toBeDefined();
+  });
+
+  it('pokemon ID less than 1 should throw error', async () => {
+    const getPokemon = pokemonService.getPokemon(0);
+
+    await expect(getPokemon).rejects.toBeInstanceOf(BadRequestException);
+  });
+
+  it('pokemon ID greater than 151 should throw error', async () => {
+    const getPokemon = pokemonService.getPokemon(152);
+
+    await expect(getPokemon).rejects.toBeInstanceOf(BadRequestException);
+  });
+
+  it('valid pokemon ID to return the pokemon name', async () => {
+    const getPokemon = pokemonService.getPokemon(1);
+
+    await expect(getPokemon).resolves.toBe('bulbasaur');
   });
 });
