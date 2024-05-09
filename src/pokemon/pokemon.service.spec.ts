88eb74bbdd025ebdf656e8109cb910daebd34bf6
kimwanyoung <dhks2869@gmail.com>
2024-05-09 18:29:01 +0900

    test: useMocker 사용하여 HttpService Mocking 테스트

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
-import { HttpModule } from '@nestjs/axios';
-import { BadRequestException } from "@nestjs/common";
+import { HttpService } from '@nestjs/axios';
+import { BadRequestException, InternalServerErrorException } from "@nestjs/common";
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 
 describe('PokemonService', () => {
   let pokemonService: PokemonService;
+  let httpService: DeepMocked<HttpService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
       providers: [PokemonService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     pokemonService = module.get<PokemonService>(PokemonService);
+    httpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
@@ -32,8 +36,34 @@ describe('PokemonService', () => {
   });
 
   it('valid pokemon ID to return the pokemon name', async () => {
+    httpService.axiosRef.mockResolvedValueOnce({
+      data: {
+        species: { name: `bulbasaur` },
+      },
+      headers: {},
+      config: { url: '' },
+      status: 200,
+      statusText: '',
+    });
+
     const getPokemon = pokemonService.getPokemon(1);
 
     await expect(getPokemon).resolves.toBe('bulbasaur');
+  });
+
+  it('if Pokemon API response unexpectedly changes, throw an error', async () => {
+    httpService.axiosRef.mockResolvedValueOnce({
+      data: `Unexpected data`,
+      headers: {},
+      config: { url: '' },
+      status: 200,
+      statusText: '',
+    });
+
+    const getPokemon = pokemonService.getPokemon(1);
+
+    await expect(getPokemon).rejects.toBeInstanceOf(
+      InternalServerErrorException,
+    );
   });
 });
